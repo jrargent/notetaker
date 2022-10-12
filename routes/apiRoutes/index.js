cbf70e054f44cc48701042a38c8ad83b0214c16b
Argent <jrargent42@gmail.com>
2022-10-11 20:59:27 -0400

    more working with GET and POST to test and get data to work how I want it

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
+const notes = require('../../db/db.json');
+//const fs = require('fs');
+//const path = require('path');
+
 
 
 router.get('/notes', (req, res) => {
-    // testing routing
+    console.log(notes);
     res.json(notes);
   });
 
   router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
+    
+    // fs.writeFileSync(
+    //   path.join(__dirname, '../../db/db.json'),
+    //   JSON.stringify(req.body, null, 2)
+    // ); 
   res.json(notes)
+  
   })
 
   module.exports = router;
